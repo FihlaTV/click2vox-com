711df78000b00d9100d2f6be8a0b6fc6c99b16fe
Giuliani Perry <perryg.88@gmail.com>
2015-10-29 00:18:19 -0600

    adding login and account updating logic, need to add auth

--- routes/index.js
+++ routes/index.js
@@ -9,17 +9,14 @@ router.get('/login', function(req, res, next){
 router.get('/signup', function(req, res, next){
   if(req.query.email){
     console.log("email present");
-    Account.findOne({email: req.query.email}, function(err, the_account){
+    Account.findOne({ email: req.query.email }, function(err, the_account){
       if(the_account){
-        console.log("found the account");
-        console.log(the_account.email);
         if(the_account.temporary == true){
           res.render('signup', { title: 'Voxbone Demo v0.1', email: req.query.email });
         }else {
           res.render('login', { title: 'Voxbone Demo v0.1', email: req.query.email });
         }
       }else{
-        console.log("attempting to create the account");
         var an_account = new Account({
           email: req.query.email,
           password: req.query.password,
@@ -27,8 +24,6 @@ router.get('/signup', function(req, res, next){
         });
 
         an_account.save(function(err) {
-          console.log("saved or error");
-          console.log(err);
           if (err) throw err;
           res.render('signup', { title: 'Voxbone Demo v0.1', email: req.query.email });
         });
@@ -39,15 +34,20 @@ router.get('/signup', function(req, res, next){
   }
 });
 
-//TODO POST /signup fetch the account with that email, set the new password and temporary to false.
+//POST /signup fetch the account with that email, set the new password and temporary to false.
 router.post('/signup', function(req, res, next){
-  console.log("entered post.....");
   var formData = req.body;
-  console.log(formData);
-  console.log("email: "+formData.email);
-  var result = { message: "this is a test", errors: null, redirect: '/login', email: formData.email }
+  var result = { message: "", errors: null, redirect: '/login', email: formData.email }
+  //TODO check if temp_password is the same as the account's password, else return error
+  Account.findOne({ email: formData.email }, function(err, the_account){
+    the_account.password = formData.password;
+    the_account.temporary = false;
+    the_account.save(function(err){
+      if(err) throw err;
+      res.status(200).json(result);
+    });
+  });
 
-  res.status(200).json(result);
 });
 
 router.get('/', function(req, res, next) {
