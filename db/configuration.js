05d48190ab9d0ec4bc5bea7932153c658453690a
Giuliani Perry <perryg.88@gmail.com>
2015-11-04 07:33:52 -0600

    removing logs in views and adding DB configuration for production env

--- db/configuration.js
+++ db/configuration.js
@@ -1,3 +1,4 @@
 var mongoose = require('mongoose');
+var dbURI = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/voxboneDB';
 
-mongoose.connect('mongodb://localhost/voxboneDB');
+mongoose.connect(dbURI);
