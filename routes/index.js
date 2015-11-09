07e8bb407d85c95d6aee897d2771fc1e706d7088
Giuliani Perry <perryg.88@gmail.com>
2015-11-09 17:28:25 -0600

    implementing forgot password, need to fix logout

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var Account = require('../models/account');
 var async = require('async');
 var title = 'Voxbone Demo v0.2';
+var crypto = require('crypto');
+var nodemailer = require('nodemailer');
 
 module.exports = function(passport){
 
@@ -13,14 +15,9 @@ module.exports = function(passport){
   router.post('/login',
     passport.authenticate('local-login'),//returns 401 unauthorized if not logged in
     function(req, res, next){
-      console.log("entered post for login");
       var formData = req.body;
       var result = { message: "", errors: null, redirect: '/widget', email: formData.email }
       res.status(200).json(result);
-  });
-
-  router.get('/forgot', function(req, res, next){
-    console.log("entered forgot password");
   });
 
   router.get('/signup', function(req, res, next){
@@ -59,6 +56,8 @@ module.exports = function(passport){
     var formData = req.body;
     var result = { message: "", errors: null, redirect: '/widget', email: formData.email }
     //TODO validate fields
+    //email regex
+    //same password and confirmation
     Account.findOne({ email: formData.email }, function(err, the_account){
       the_account.password = the_account.generateHash(formData.password);
       the_account.temporary = false;
@@ -83,6 +82,129 @@ module.exports = function(passport){
 
   router.get('/', function(req, res, next) {
     res.render('login', { title: title, account: accountLoggedIn(req) });
+  });
+
+  router.get('/forgot', function(req, res, next){
+    res.render('forgot', { title: title, email: req.query.email, account: accountLoggedIn(req) });
+  });
+
+  router.post('/forgot', function(req, res, next) {
+    async.waterfall([
+      function(done) {
+        crypto.randomBytes(20, function(err, buf) {
+          var token = buf.toString('hex');
+          done(err, token);
+        });
+      },
+      function(token, done) {
+        Account.findOne({ email: req.body.email }, function(err, account) {
+          if (!account) {
+            var result = { message: "No account with that email address exists.", errors: err }
+            return res.status(400).json(result);
+          }
+
+          account.resetPasswordToken = token;
+          account.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+          account.save(function(err) {
+            done(err, token, account);
+          });
+        });
+      },
+      function(token, account, done) {
+        var smtpTransport = nodemailer.createTransport('SMTP', {
+          service: 'SendGrid',
+          auth: {
+            user: process.env.USER,
+            pass: process.env.PASSWORD
+          }
+        });
+        var mailOptions = {
+          to: account.email,
+          from: process.env.FROM,
+          subject: 'Voxbone Widget Generator - Password Reset',
+          text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+            'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+            'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+            'If you did not request this, please ignore this email and your password will remain unchanged.\n',
+          html: "<h2> Password Reset </h2> <p>You are receiving this because you (or someone else) have requested the reset of the password for your account.<br/> Please click on the following link, or paste this into your browser to complete the process: <br/> http://"+ req.headers.host + "/reset/" + token +"<br/> If you did not request this, please ignore this email and your password will remain unchanged. </p>"
+        };
+        smtpTransport.sendMail(mailOptions, function(err) {
+          if(err){
+            done(err, 'done');
+          }else{
+            var result = { message: "An e-mail has been sent to " + account.email + " with further instructions.", errors: null }
+            res.status(200).json(result);
+          }
+        });
+      }
+    ], function(err) {
+      if (err) return next(err);
+      var result = { message: "There was an error", errors: err }
+      res.status(400).json(result);
+    });
+  });
+
+  router.get('/reset/:token', function(req, res) {
+    Account.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, account) {
+      if (!account) {
+        return res.render('forgot', { title: title, message: "Password reset token is invalid or has expired.", errors: err })
+      }
+      res.render('reset', { title: title, account: accountLoggedIn(req), the_account: req.user, token:req.params.token });
+    });
+  });
+
+  router.post('/reset/:token', function(req, res) {
+    async.waterfall([
+      function(done) {
+        Account.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, account) {
+          if (!account) {
+            var result = { message: "Password reset token is invalid or has expired.", errors: err }
+            return res.status(400).json(result);
+          }
+
+          //TODO validate password and confirmation
+          account.password = req.body.password;
+          account.resetPasswordToken = undefined;
+          account.resetPasswordExpires = undefined;
+
+          account.save(function(err) {
+            req.logIn(account, function(err) {
+              done(err, account);
+            });
+          });
+        });
+      },
+      function(account, done) {
+        var smtpTransport = nodemailer.createTransport('SMTP', {
+          service: 'SendGrid',
+          auth: {
+            user: process.env.USER,
+            pass: process.env.PASSWORD
+          }
+        });
+        var mailOptions = {
+          to: account.email,
+          from: process.env.FROM,
+          subject: 'Your password has been changed',
+          text: 'Hello,\n\n' +
+            'This is a confirmation that the password for your account ' + account.email + ' has just been changed.\n',
+          html: "<h2> Hello </h2> <p> This is a confirmation that the password for your account " +account.email+" has just been changed.</p>"
+        };
+        smtpTransport.sendMail(mailOptions, function(err) {
+          req.flash('success', 'Success! Your password has been changed.');
+          done(err);
+        });
+      }
+    ], function(err) {
+      if (err){
+        var result = { message: "An error has ocurred.", errors: err }
+        return res.status(400).json(result);
+      }else{
+        var result = { message: "Your password has been changed.", errors: null, redirect: '/widget' }
+        return res.status(200).json(result);
+      }
+    });
   });
 
   function isLoggedIn(req, res, next) {
