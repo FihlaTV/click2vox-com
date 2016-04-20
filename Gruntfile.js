9e7b9cee0bf9fa4c7b81454a4253d9fb762b32c2
Helmy Giacoman <helmygb@gmail.com>
2016-04-20 14:07:06 -0600

    [MISC] Load config sync mode and login adjustments

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,5 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
-    bowerRequirejs: {
-      target: {
-        rjsConfig: 'public/config.js'
-      }
-    }
   });
-
-  grunt.loadNpmTasks('grunt-bower-requirejs');
 }
