5b2cfb9965cd28e4460c427f25e506764ffdccb7
Helmy Giacoman <helmygb@gmail.com>
2016-05-04 20:44:27 -0600

    [vxb-html-2] Styles and structure improvements (#145)
    * folder removal
    
    * button list moks page
    
    * new sip modal call
    
    * error alert style
    
    * header fix
    
    * [vxb-html-2] Integrating grunt task to compile less to css
    
    * small ui fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,47 @@
 module.exports = function (grunt) {
+  require('jit-grunt')(grunt);
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "public/stylesheets/root.css": "public/stylesheets/root.less",
+          "public/stylesheets/custom.css": "public/stylesheets/custom.less"
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['public/stylesheets/**/*.less'],
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    },
+    nodemon: {
+      dev: {
+        script: './bin/www'
+      }
+    },
+    concurrent: {
+      devel: {
+        tasks: ['nodemon', 'watch'],
+        options: {
+            logConcurrentOutput: true
+        }
+      }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent');
+
+  grunt.registerTask('default', ['concurrent:devel']);
 }
