9db544db853e2b25fc2a6b2b055f29246566ce13
Helmy Giacoman <helmygb@gmail.com>
2016-06-07 14:07:19 -0600

    [#201] Password reset error on staging (#204)

--- public/javascripts/directives/password.check.js
+++ public/javascripts/directives/password.check.js
@@ -4,11 +4,10 @@ define(function () {
       require: 'ngModel',
       link: function (scope, elem, attrs, ctrl) {
         var firstPassword = '#' + attrs.pwCheck;
-        elem.add(firstPassword).on('keyup', function () {
-          scope.$apply(function () {
-            var pwVerify = elem.val() === $(firstPassword).val();
-            ctrl.$setValidity('pwmatch', pwVerify);
-          });
+        ctrl.$parsers.unshift(function (value) {
+          var pwVerify = value === $(firstPassword).val();
+          ctrl.$setValidity('pwmatch', pwVerify);
+          return value;
         });
       }
     };
