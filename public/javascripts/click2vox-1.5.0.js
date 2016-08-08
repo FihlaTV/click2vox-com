f4e7e2279a27ea7b60ac92695edf8626d7cd21e1
Santiago Traversa <santiago.traversa@gmail.com>
2016-08-08 16:19:04 -0300

    [MISC] Fixes last commit

--- public/javascripts/click2vox-1.5.0.js
+++ public/javascripts/click2vox-1.5.0.js
@@ -8,7 +8,7 @@ if (voxButtonElement === undefined) {
   voxButtonElement = document.createElement("div");
   voxButtonElement.className = "voxButton";
   voxButtonElement.dataset.use_default_button_css = false;
-  document.body.appendChild(voxButtonElement);
+  // document.body.appendChild(voxButtonElement);
 }
 
 function loadScript(url, callback) {
