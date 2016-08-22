389984ca707e752f114f0509849a67613ed9b62d
Santiago Traversa <san983@users.noreply.github.com>
2016-08-22 13:20:57 -0300

    [FIX] Fixes opening an URL on non-webrtc preview mode (#378)

--- public/javascripts/controllers/widget.mixin.js
+++ public/javascripts/controllers/widget.mixin.js
@@ -3,6 +3,13 @@ define(function(Clipboard) {
   var WidgetControllerMixin = function($scope) {
 
     $scope.makeCall = function(did) {
+      var ibc_value = $scope.widget.incompatible_browser_configuration;
+      if (!$scope.preview_webrtc_compatible && (ibc_value === 'link_button_to_a_page')) {
+        var redirect_url = $scope.widget.link_button_to_a_page_value || 'https://voxbone.com';
+        window.open(redirect_url);
+        return;
+      };
+
       var voxButtonElement = document.getElementById('voxButtonPreview');
 
       var didToCall = (typeof did === 'undefined') ? $scope.did : did;
