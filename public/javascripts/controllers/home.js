755f76152c39946401a70e8650c0f987b3e2754a
Santiago Traversa <san983@users.noreply.github.com>
2017-02-21 17:39:16 -0300

    [#385] [#585] Playing DTMF tones (#592)
    * [#385] Playing DTMF tones
    
    * [#385] Captures keyboard events for Dialpad

--- public/javascripts/controllers/home.js
+++ public/javascripts/controllers/home.js
@@ -2,11 +2,11 @@ define(['angular', 'jquery'], function (angular, $) {
   var HomeController = function ($scope, $http, $window) {
     $scope.callVoxbone = function () {
       $('#launch_call').click();
-    }
+    };
 
     $scope.showCallVoxboneButton = function () {
       return voxbone.WebRTC.isWebRTCSupported();
-    }
+    };
   };
 
   var clearContactForm = function () {
